bd46e3ff6865180cf7575867766d9fb4da283aa1
Zerldas <17.12a15.nguyenhuuanhkhoa@gmail.com>
2025-11-25 20:45:51 +0700

    Update url backend

--- frontend-react/src/config/api.js
+++ frontend-react/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5454/api";
+export const API_URL = process.env.REACT_APP_API_URL || "https://unegoistically-semestrial-keven.ngrok-free.dev/api";
 
 export const api = axios.create({
   baseURL: API_URL,
